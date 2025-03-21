13c0757caf09ef96f70520db2287ba64a8c3ccff
samcode-dev <samueloluwatimileyin8@gmail.com>
2025-03-21 19:09:32 +0100

    containarizing it using docker and adding documentation

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,16 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      port: 5173,
+    },
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
